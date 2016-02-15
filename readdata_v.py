4ca3d13dbc69dbbfda29a03e7d45264c4ae7871b
Nick Attree <nickattree@gmail.com>
2016-02-15 17:07:07 +0000

    Update readdata_v.py

--- readdata_v.py
+++ readdata_v.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import math as m
 
-stuff = np.genfromtxt('positions_run1.txt', dtype = None, names = "dt, id, x, y, z, vx, vy, vz")
+stuff = np.genfromtxt('positions.txt', dtype = None, names = "dt, id, x, y, z, vx, vy, vz")
 number = max(stuff['id'])+1
 timesteps = stuff.size/number
 
@@ -17,21 +17,15 @@ vz = data['vz']
 
 for i in range (0, timesteps-1) :
 	fig = plt.figure(figsize=(8,8))
-  ax = plt.subplot(111,aspect='equal')
-  #ax.set_ylim(-4,4)
-  #ax.set_xlim(-4,4)
-	#for j in range(0, number-1):
-      #	circ = patches.Circle((y[i,j], x[i,j]), 130, facecolor='darkgray', edgecolor='black')
-      #	ax.add_patch(circ)
+	ax = plt.subplot(111,aspect='equal')
+	#ax.set_ylim(-4,4)
+	#ax.set_xlim(-4,4)
 
 	plt.plot(vy[i,:], vx[i,:], 'kp')
 	#plt.show()
 
 	plt.savefig("%d.png" % (i))
 	plt.close()
-
-# Plot single particle's trajectory
-# plt.plot(y[np.where(id == 0)], x[np.where(id == 0)])
 
 i = 24
 v = np.sqrt(vx[i,:]**2.0 + vy[i,:]**2.0 + vz[i,:]**2.0)
